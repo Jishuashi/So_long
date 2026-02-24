defd3c70eb0482a2f546ff7668840cbe67192164
hchartie <hchartie@student.42.fr>
2026-02-24 12:57:54 +0100

    Add all include

--- so_long.h
+++ so_long.h
@@ -6,11 +6,15 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 12:01:00 by hchartie          #+#    #+#             */
-/*   Updated: 2026/02/24 12:03:40 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/02/24 12:57:37 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef  SO_LONG_H
 # define SO_LONG_H
 # include "./libft/libft.h"
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
+# include <math.h>
 #endif
